3d1e29b55453aa43deb411ad32cd5c6b51f46d6e
alireza <alirezabg@gmail.com>
2023-01-16 13:52:11 +0000

    update frontend sh

--- terraform/frontend.sh
+++ terraform/frontend.sh
@@ -3,7 +3,7 @@ sudo apt install --yes docker-ce
 sudo usermod -a -G docker ${USER}
 gcloud auth configure-docker us-central1-docker.pkg.dev
 docker pull \
-    us-central1-docker.pkg.dev/planar-elevator-374616/quickstart-docker-repo/quickstart-image@sha256:5c399fa1c83c3103a33338ffeb16bf16530c3902241b82b44e75b9119e65a586
-sudo docker pull us-central1-docker.pkg.dev/planar-elevator-374616/quickstart-docker-repo/quickstart-image:tag1
-docker tag $(docker images "us-central1-docker.pkg.dev/planar-elevator-374616/quickstart-docker-repo/quickstart-image" -q) api:v1
-sudo docker run -p 4000:4000 api:v1
+    us-central1-docker.pkg.dev/planar-elevator-374616/client/client-image@sha256:31513e2ef059ced5884f9564134eb56f632d2889ec1f249179969abbeb200636
+sudo docker pull us-central1-docker.pkg.dev/planar-elevator-374616/client/client-image:tag1
+docker tag $(docker images "us-central1-docker.pkg.dev/planar-elevator-374616/client/client-image" -q) client:v1
+sudo docker run -p 3000:3000 client:v1
